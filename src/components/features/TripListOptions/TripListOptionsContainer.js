d0e7f09b1b790098ca34280dbe77a0471ca04cf0
Bartłomiej Piskulla <bartlomiej.piskulla@gmail.com>
2020-06-09 22:26:20 +0200

    Add filtering by duration and tags

--- src/components/features/TripListOptions/TripListOptionsContainer.js
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -1,7 +1,7 @@
 import {connect} from 'react-redux';
 import TripListOptions from './TripListOptions';
 import {getAllTags} from '../../../redux/tagsRedux';
-import {getAllFilters, changeSearchPhrase} from '../../../redux/filtersRedux';
+import { getAllFilters, changeSearchPhrase, changeDuration, addTagToFilter, removeTagToFilter } from '../../../redux/filtersRedux';
 
 const mapStateToProps = state => ({
   tags: getAllTags(state),
@@ -11,6 +11,9 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
   // TODO - add more dispatchers for other filters
+  changeDuration: duration => dispatch(changeDuration(duration)),
+  addTagToFilter: (tag, checked) => dispatch(addTagToFilter(tag, checked)),
+  removeTagToFilter: (tag, checked) => dispatch(removeTagToFilter(tag, checked)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -11,10 +11,16 @@ const createActionName = name => `app/${reducerName}/${name}`;
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
 // TODO - add other action types
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
+export const ADD_TAG_TO_FILTER = createActionName('ADD_TAG_TO_FILTER');
+export const REMOVE_TAG_TO_FILTER = createActionName('REMOVE_TAG_TO_FILTER');
 
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
 // TODO - add other action creators
+export const changeDuration = payload => ({ payload, type: CHANGE_DURATION });
+export const addTagToFilter = payload => ({ payload, type: ADD_TAG_TO_FILTER });
+export const removeTagToFilter = payload => ({ payload, type: REMOVE_TAG_TO_FILTER });
 
 // reducer
 export default function reducer(statePart = [], action = {}) {
@@ -25,6 +31,29 @@ export default function reducer(statePart = [], action = {}) {
         searchPhrase: action.payload,
       };
     // TODO - handle other action types
+    case CHANGE_DURATION:
+      return {
+        ...statePart,
+        duration: {
+          ...statePart.duration,
+          [action.payload.type]: parseInt(action.payload.value),
+        },
+      };
+    case ADD_TAG_TO_FILTER:
+      return {
+        ...statePart,
+        tags: [
+          ...statePart.tags,
+          action.payload.tag,
+        ],
+      };
+    case REMOVE_TAG_TO_FILTER:
+      return {
+        ...statePart,
+        tags: [
+          ...statePart.tags.filter(tag => tag != action.payload.tag),
+        ],
+      };
     default:
       return statePart;
   }
